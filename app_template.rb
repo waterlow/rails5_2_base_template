c32f2c46c9f92423e249c290bc8d11af8e0171c1
waterlow <waterlow3442@gmail.com>
2018-04-16 00:15:20 +0900

    bootstrapのインストール

--- app_template.rb
+++ app_template.rb
@@ -3,6 +3,8 @@ require 'bundler'
 gem 'hamlit'
 gem 'babel-transpiler'
 gem 'sprockets', '4.0.0.beta7'
+gem 'bootstrap'
+gem 'jquery-rails'
 gem_group :development do
   gem 'hamlit-rails'
   gem 'erb2haml'
@@ -14,3 +16,17 @@ Bundler.with_clean_env do
 end
 
 gsub_file 'Gemfile', /  gem 'erb2haml'\n/, ''
+
+run 'rm -rf app/assets/stylesheets/application.css'
+get(
+  'https://raw.github.com/morizyun/rails5_application_template/master/app/assets/stylesheets/application.scss',
+  'app/assets/stylesheets/application.scss'
+)
+
+insert_into_file 'app/assets/javascripts/application.js', %(
+  //= require jquery3
+  //= require popper
+  //= require bootstrap-sprockets
+), before: '//= require_tree .'
+
+gsub_file 'Gemfile', /\/\/= require_tree .\n/, ''
